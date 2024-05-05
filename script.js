aba97505f628fb936d46e60126d17aaeb9950ef0
Vivek Vinod <vivekvinodchess@gmail.com>
2024-05-05 18:49:02 +0530

    refactor code

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 const container = document.querySelector("#container");
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.style.cssText = `
-        display: flex; 
-        justify-content: center;
-        flex: 1;
-    `;
-    container.appendChild(row);
-    for (let j = 0; j < 16; j++) {
-        let div = document.createElement("div");
-        div.style.cssText = `
+function createGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.style.cssText = `
+            display: flex; 
+            justify-content: center;
             flex: 1;
-            max-width: 40px;
-            aspect-ratio: 1/1;
-            border: 1px solid black;
         `;
-        div.addEventListener("mouseover", () => {
-            div.style.backgroundColor = "black";
-        })
-        row.appendChild(div);
+        container.appendChild(row);
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement("div");
+            div.style.cssText = `
+                flex: 1;
+                max-width: 40px;
+                aspect-ratio: 1/1;
+                border: 1px solid black;
+            `;
+            div.addEventListener("mouseover", () => {
+                div.style.backgroundColor = "black";
+            })
+            row.appendChild(div);
+        }
     }
 }
+createGrid(16);
