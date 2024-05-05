1c6be30cb34938cfeaae86cc676e36f303a4191b
Vivek Vinod <vivekvinodchess@gmail.com>
2024-05-05 17:31:25 +0530

    create basic ui

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector("#container");
+for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.style.cssText = `
+        display: flex; 
+        justify-content: center;
+        flex: 1;
+    `;
+    container.appendChild(row);
+    for (let j = 0; j < 16; j++) {
+        let div = document.createElement("div");
+        div.style.cssText = `
+            flex: 1;
+            max-width: 40px;
+            aspect-ratio: 1/1;
+            border: 1px solid black;
+        `;
+        row.appendChild(div);
+    }
+}
